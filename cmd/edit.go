ceeea749ce4df4123be4e82f0e1f22dc1ffdf799
John Gallagher <john@synapticmishap.co.uk>
2024-12-19 13:14:16 +0000

    wip: basic implementation of command line args

---
+++ cmd/edit.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// editCmd represents the edit command
+var editCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("edit called")
+	},
+}
+
+func init() {
+	pageCmd.AddCommand(editCmd)
+	editCmd.Flags().StringP("file", "f", "src/index.md", "Path to the markdown file")
+}
